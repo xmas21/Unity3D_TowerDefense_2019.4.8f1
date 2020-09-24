43853028166d52f2f3a04a480df30b48a586c20a
HWChia <66512878+xmas21@users.noreply.github.com>
2020-09-25 02:50:08 +0800

    遊戲選單 | 選單控制器 | 防禦塔&怪物剛體與碰撞 | 素材確定

---
+++ TowerDefanse/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("載入畫面")]
+    public GameObject panelloading;
+    [Header("進度")]
+    public Text textloading;
+    [Header("提示文字")]
+    public GameObject tip;
+    [Header("進度條")]
+    public Image imgloading;
+    [Header("載入場景名稱")]
+    public string nameScene = "遊戲畫面";
+
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(Loading());
+    }
+
+
+    public IEnumerator Loading()
+    {
+        panelloading.SetActive(true);
+        AsyncOperation ao = SceneManager.LoadSceneAsync(nameScene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            textloading.text = (ao.progress / 0.9f * 100).ToString("F0") + "%";
+            imgloading.fillAmount = ao.progress;
+            yield return null;
+
+            if (ao.progress == 0.9f)
+            {
+                tip.SetActive(true);
+
+                if (Input.anyKeyDown)
+                {
+                    ao.allowSceneActivation = true;
+                }
+            }
+
+
+        }
+    }
+}
